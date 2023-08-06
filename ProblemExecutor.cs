cf992e397160a89c3d4647b613aec619c98ba11e
Панфилов Паша <pv.forwork@gmail.com>
2023-08-06 15:36:28 +0300

    Решил задачу #2(определение слов-анаграмм).

--- ProblemExecutor.cs
+++ ProblemExecutor.cs
@@ -29,5 +29,56 @@ namespace LeetCodeProblems
 
             return false;
         }
+
+        /// <summary>
+        /// 2. Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+        /// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+        /// </summary>
+        internal static bool IsAnagram(string s, string t)
+        {
+            // Не имеет смысла продолжать проверку на анаграмму, если строки изначально имеют разную длину.
+            if (s.Length != t.Length)
+            {
+                return false;
+            }
+
+            Dictionary<char, int> charsCount = new();
+
+            // Проходимся сразу по обеим строкам одним циклом:
+            // для первой коллекции увеличиваем счетчик символа,
+            // а для второй - уменьшаем.
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (charsCount.ContainsKey(s[i]))
+                {
+                    charsCount[s[i]]++;
+                }
+                else
+                {
+                    charsCount.Add(s[i], 1);
+                }
+
+                if (charsCount.ContainsKey(t[i]))
+                {
+                    charsCount[t[i]]--;
+                }
+                else
+                {
+                    charsCount.Add(t[i], -1);
+                }
+            }
+
+            // В результате имеем словарь, в котором ключом является символ, а значением - "разница" между количеством его нахождений во входных строках.
+            // Если эта разница не равна 0, то кол-во этих символов не одинаково, и данные строки не являются анаграммами.
+            foreach (int charCount in charsCount.Values)
+            {
+                if (charCount != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ProblemExecutor.IsAnagram("rat", "car");
+
+            Console.ReadKey();
         }
     }
 }
